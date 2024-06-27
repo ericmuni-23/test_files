cc1bfe6f472fe7883de7e19c81a144a8bc0f7b78
ericmuni-23 <munieric44@gmail.com>
2024-06-27 23:38:26 +0300

    this is a test file

---
+++ statements_Elif_Else_If/statements.py
@@ -0,0 +1,9 @@
+temparature = 15
+if temparature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temparature > 20:
+    print("it's nice")  
+else:
+    print("it's cold")     
+print("Done") 
